3da76b025e297c548d70aa9a48b75c320dca4b78
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-22 14:42:44 +0100

    create formula with subtraction

--- js/script.js
+++ js/script.js
@@ -9,6 +9,17 @@
 // }
 // alert(goup());
 
+// let a = 1;
+// let b = 5;
+// let c = 6;
+
+// function goup(){
+//     return (-b - Math.sqrt(b**2 - 4 * a * c))/ (2 * a);
+// }
+// alert(goup());
+
+
+
 
 // a= 2, c= 6, d= 4, b= 3 ((a*c + b*d)/(c**2 + d**2)) + ((b*c - a*d)/(c**2 + d**2))
 // let a = 2;
@@ -20,13 +31,14 @@
 //     let formula = (a * c + b * d)/(c ** 2 + d **2) + (b * c - a * d)/(c ** 2 + d ** 2);
 //     return formula;
 // }
-// // alert(goout());
+// alert(goout());
 // console.log(goout());
 
 // where y= 3, m = (y**2 - 32)**2 / Math.sqrt(4/y)
-let y = 3;
+// let y = 3;
 
-function goin(){
-     return (y ** 2 - 32)**2 / math.sqrt(4/y);
-}
-alert(goin());
+// function goin(){
+
+//      return (y ** 2 - 32)**2 / Math.sqrt(4/y);
+// }
+// alert(goin());
