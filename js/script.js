cc4bd59acffa68e06e188423a0e615619288e2da
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-22 14:00:11 +0100

    create formula to solve mathimatical expression

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// b= 5, a= 1, c=6 (-b +- Math.sqrt(b**2 - (4*a*c)/2a)
+// let m = parseInt(prompt());
+// let a = 1;
+// let b = 5;
+// let c = 6;
+
+// function goup(){
+//     return (-b + Math.sqrt(b**2 - 4 * a * c))/ (2 * a);
+// }
+// alert(goup());
+
+
+// a= 2, c= 6, d= 4, b= 3 ((a*c + b*d)/(c**2 + d**2)) + ((b*c - a*d)/(c**2 + d**2))
+// let a = 2;
+// let b = 3;
+// let c = 6;
+// let d = 4;
+
+// function goout(){
+//     let formula = (a * c + b * d)/(c ** 2 + d **2) + (b * c - a * d)/(c ** 2 + d ** 2);
+//     return formula;
+// }
+// // alert(goout());
+// console.log(goout());
+
+// where y= 3, m = (y**2 - 32)**2 / Math.sqrt(4/y)
+let y = 3;
+
+function goin(){
+     return (y ** 2 - 32)**2 / math.sqrt(4/y);
+}
+alert(goin());
