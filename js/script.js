b890649e063dc5e2ce74c24d533fd1546811fdda
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-24 20:52:13 +0100

    solve another formula with a new function and arrangement

--- js/script.js
+++ js/script.js
@@ -85,20 +85,36 @@
 // q3
 // // a= 2, c= 6, d= 4, b= 3 ((a*c + b*d)/(c**2 + d**2)) + ((b*c - a*d)/(c**2 + d**2))
 
+// let m = parseInt(prompt());
+
+// let a = 2;
+// let b = 3;
+// let c = 6;
+// let d = 4;
+
+// function formL(){
+//      let eqt1 = a*c + b*d;
+//      let eqt2 = b*c - a*d;
+//      let eqt3 = eqt1 + eqt2;
+//      let x = Math.round(eqt3)/(c**2 + d**2);
+
+//      return x;
+// }
+// document.write(formL());
+
+
+
+// q4
+// // where y= 3, m = (y**2 - 32)**2 / Math.sqrt(4/y)
 let m = parseInt(prompt());
+let y = 3;
 
-let a = 2;
-let b = 3;
-let c = 6;
-let d = 4;
-
-function formL(){
-     let eqt1 = a*c + b*d;
-     let eqt2 = b*c - a*d;
-     let eqt3 = eqt1 + eqt2;
-     let x = Math.round(eqt3)/(c**2 + d**2)
+function quadE(){
+     let eqt1 = (y**2 - 32)**2;
+     let eqt2 = Math.sqrt(4/y);
+     let x = Math.round(eqt1 / eqt2);
 
      return x;
 }
-document.write(formL());
+document.write(quadE());
 
